1a6577439e21b4a8d753eb9a22a7cf9b46805e0b
jkh416 <jkh416@lehigh.edu>
2017-11-03 02:04:41 -0400

    Initial commit

---
+++ app/about-me/index.controller.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('About-me.IndexController', Controller);
+
+    function Controller(UserService) {
+        var vm = this;
+
+        vm.user = null;
+        vm.btn = 'I Am Button';
+        vm.showDarkTheme = true;
+        initController();
+
+        function initController() {
+            // get current user
+            UserService.GetCurrent().then(function (user) {
+                vm.user = user;
+            });
+        }
+    }
+
+})();
